7dc3c63f62a70d302eb32881dd09356f91f6ac8f
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-07-16 14:24:04 +0700

    Update tools.py

--- utils/tools.py
+++ utils/tools.py
@@ -1,1 +1,29 @@
+import numpy as np
+import os
+import io
+import math
+import random
+import pandas as pd
 
+import matplotlib.pyplot as plt
+import librosa
+import librosa.display
+import cv2
+import cmapy
+
+import nlpaug
+import nlpaug.augmenter.audio as naa
+
+from scipy.signal import butter, lfilter
+
+def butter_bandpass(lowcut, highcut, fs, order=5):
+  nyq = 0.5 * fs
+  low = lowcut/nyq
+  high = highcut/nyq
+  b, a = butter(order, [low, high], btype='band')
+  return b, a
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+  b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+  y = lfilter(b, a, data)
+  return y
