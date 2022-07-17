f0409ee85ddb8edb281d9bd40f778e02acb0a66d
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-07-17 19:02:27 +0700

    Create CNN.py

---
+++ nets/CNN.py
@@ -0,0 +1,48 @@
+import tensorflow as tf
+
+# link Neural Network: https://keras.io/api/applications/
+
+def EfficientNetV2M(image_length=224, training=False)
+  inputs = keras.Input(shape=(image_length, image_length, 1))
+  base_model = tf.keras.applications.EfficientNetV2M(include_top=False,
+                                                      weights="imagenet",
+                                                      input_shape=(224, 224, 1)
+                                                      classes=4,
+                                                      classifier_activation="softmax",
+                                                      include_preprocessing=True,)
+  x = base_model(inputs, training=training)
+  return x
+
+def NASNetLarge(image_length=224, training=False)
+  inputs = keras.Input(shape=(image_length, image_length, 1))
+  base_model = tf.keras.applications.NASNetLarge(include_top=False,
+                                                      weights="imagenet",
+                                                      input_shape=(224, 224, 1)
+                                                      classes=4,
+                                                      classifier_activation="softmax",
+                                                      include_preprocessing=True,)
+  x = base_model(inputs, training=training)
+  return x
+
+def InceptionResNetV2(image_length=224, training=False)
+  inputs = keras.Input(shape=(image_length, image_length, 1))
+  base_model = tf.keras.applications.InceptionResNetV2(include_top=False,
+                                                      weights="imagenet",
+                                                      input_shape=(224, 224, 1)
+                                                      classes=4,
+                                                      classifier_activation="softmax",
+                                                      include_preprocessing=True,)
+  x = base_model(inputs, training=training)
+  return x
+
+def ResNet152V2(image_length=224, training=False)
+  inputs = keras.Input(shape=(image_length, image_length, 1))
+  base_model = tf.keras.applications.ResNet152V2(include_top=False,
+                                                      weights="imagenet",
+                                                      input_shape=(224, 224, 1)
+                                                      classes=4,
+                                                      classifier_activation="softmax",
+                                                      include_preprocessing=True,)
+  x = base_model(inputs, training=training)
+  return x
+  
