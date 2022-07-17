60c8975ca48632136532157bfe8ae4b6f4f0b455
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-07-17 22:55:48 +0700

    Update tools.py

--- utils/tools.py
+++ utils/tools.py
@@ -10,6 +10,7 @@ import librosa
 import cv2
 
 import pickle as pkl
+from keras import backend as K
 
 def slice_data(start, end, raw_data, sample_rate):
     max_ind = len(raw_data) 
@@ -92,3 +93,20 @@ def create_spectrograms_raw(current_window, sample_rate=4000, n_mels=224, f_min=
     img = np.expand_dims(img, axis=-1)
     img = np.expand_dims(img, axis=0)
     return img
+
+def recall_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+def precision_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+def f1_m(y_true, y_pred):
+    precision = precision_m(y_true, y_pred)
+    recall = recall_m(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
