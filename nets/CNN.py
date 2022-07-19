203ca5dbc8781253ae9d75f0e24c26c4d0a2617f
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-07-19 16:04:58 +0700

    Update CNN.py

--- nets/CNN.py
+++ nets/CNN.py
@@ -5,46 +5,49 @@ from tensorflow.keras.models import Model
 # link Neural Network: https://keras.io/api/applications/
 
 def EfficientNetV2M(image_length=224, training=False):
-  inputs = keras.Input(shape=(image_length, image_length, 1))
-  base_model = tf.keras.applications.EfficientNetV2M(include_top=True,
-                                                      weights=None,
-                                                      input_shape=(224, 224, 1),
-                                                      classes=4,
-                                                      classifier_activation="softmax")
-  output = base_model(inputs, training=training)
-  x = Model(inputs, output)
-  return x
+    inputs = keras.Input(shape=(image_length, image_length, 1))
+    base_model = tf.keras.applications.EfficientNetV2M(include_top=True,
+                                                        weights=None,
+                                                        input_shape=(224, 224, 1),
+                                                        classes=4,
+                                                        classifier_activation="softmax")
+    output = base_model(inputs, training=training)
+    x = Model(inputs, output)
+    return x
 
-def DenseNet201(image_length=224, training=False):
-  inputs = keras.Input(shape=(image_length, image_length, 1))
-  base_model = tf.keras.applications.DenseNet201(include_top=True,
-                                                      weights=None,
-                                                      input_shape=(224, 224, 1),
-                                                      classes=4,
-                                                      classifier_activation='softmax')
-  output = base_model(inputs, training=training)
-  x = Model(inputs, output)
-  return x
+
 
 def InceptionResNetV2(image_length=224, training=False):
-  inputs = keras.Input(shape=(image_length, image_length, 1))
-  base_model = tf.keras.applications.InceptionResNetV2(include_top=True,
-                                                      weights=None,
-                                                      input_shape=(224, 224, 1),
-                                                      classes=4,
-                                                      classifier_activation='softmax')
-  output = base_model(inputs, training=training)
-  x = Model(inputs, output)
-  return x
+    inputs = keras.Input(shape=(image_length, image_length, 1))
+    base_model = tf.keras.applications.InceptionResNetV2(include_top=True,
+                                                        weights=None,
+                                                        input_shape=(224, 224, 1),
+                                                        classes=4,
+                                                        classifier_activation='softmax')
+    output = base_model(inputs, training=training)
+    x = Model(inputs, output)
+    return x
+
+def MobileNetV2(image_length=224, training=False):
+    inputs = keras.Input(shape=(image_length, image_length, 1))
+    base_model = tf.keras.applications.MobileNetV2(include_top=True,
+                                                    weights=None,
+                                                    input_shape=(224, 224, 1),
+                                                    classes=4,
+                                                    classifier_activation="softmax",
+                                                )
+    output = base_model(inputs, training=training)
+    x = Model(inputs, output)
+    return x
 
 def ResNet152V2(image_length=224, training=False):
-  inputs = keras.Input(shape=(image_length, image_length, 1))
-  base_model = tf.keras.applications.ResNet152V2(include_top=True,
-                                                      weights=None,
-                                                      input_shape=(224, 224, 1),
-                                                      classes=4,
-                                                      classifier_activation='softmax')
-  output = base_model(inputs, training=training)
-  x = Model(inputs, output)
-  return x
+    inputs = keras.Input(shape=(image_length, image_length, 1))
+    base_model = tf.keras.applications.ResNet152V2(include_top=True,
+                                                        weights=None,
+                                                        input_shape=(224, 224, 1),
+                                                        classes=4,
+                                                        classifier_activation='softmax')
+    output = base_model(inputs, training=training)
+    x = Model(inputs, output)
+    return x
   
