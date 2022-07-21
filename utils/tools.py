38d007364da9f9365ab4f469c33227fa0d232089
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-07-21 22:12:13 +0700

    Update tools.py

--- utils/tools.py
+++ utils/tools.py
@@ -13,13 +13,14 @@ from sklearn.metrics import confusion_matrix, accuracy_score
 import pickle as pkl
 from keras import backend as K
 
+# load data from start time to end time in each audio file
 def slice_data(start, end, raw_data, sample_rate):
     max_ind = len(raw_data) 
     start_ind = min(int(start * sample_rate), max_ind)
     end_ind = min(int(end * sample_rate), max_ind)
     return raw_data[start_ind: end_ind]
 
-# [normal, crackle, wheeze, both] == [0, 1, 2, 3]
+# label: [normal, crackle, wheeze, both] == [0, 1, 2, 3]
 def get_label(crackle, wheeze):
     if crackle == 0 and wheeze == 0:
         return 0
@@ -29,7 +30,8 @@ def get_label(crackle, wheeze):
         return 2
     else:
         return 3
-      
+
+# read .txt file to get annotations
 def get_annotations(file_name, data_dir):
   # file_name: .txt file
   f = open(os.path.join(data_dir, file_name), "r")
@@ -37,7 +39,10 @@ def get_annotations(file_name, data_dir):
   annotations = np.array([i.split('\t') for i in f1], dtype=np.float32)
   f.close()
   return annotations
-      
+
+# label consists of its data
+# for example: ['0': [...], '1': [...], '2': [...], '3': [...]]
+# data of each label is in ...
 def get_sound_samples(labels_data, annotations, file_name, data_dir, sample_rate=4000):
     # annotations: - type: list
     #              - construction: Each row of annotations is [..., ..., ..., ...] == [start, end, crackles, wheezes]
@@ -53,32 +58,38 @@ def get_sound_samples(labels_data, annotations, file_name, data_dir, sample_rate
         crackles = row[2]
         wheezes = row[3]
         
-        audio_chunk = slice_data(start, end, data, rate)
-        label_name = get_label(crackles, wheezes)
-        labels_data[label_name].append(audio_chunk)
+        audio_chunk = slice_data(start, end, data, rate) # get segment data based on start and end time
+        label_name = get_label(crackles, wheezes) # get label
+        labels_data[label_name].append(audio_chunk) # put data belong to its label
     return labels_data
-          
+
+# load .pkz file
 def load_df(pkz_file):
     with open(pkz_file, 'rb') as f:
         df=pkl.load(f)
     return df
 
+# save .pkz file
 def save_df(df, out_file):
   with open(out_file, 'wb') as pfile:
     pkl.dump(df, pfile)
     print('{0} saved'.format(out_file))
-    
+
+# convert labels to one-hot type
 def to_onehot(x, num=4):
     a = np.zeros((num, ))
     a[x] = 1
     return a.tolist()
 
+# Convert 1D-raw data to image by stft
+# w, h: width, height
+# width = fft_length/2 = 224
 def create_stft(current_window, frame_length=255, frame_step=100, fft_length=224*2): # increase hop -> decrease height of image
     S = tf.signal.stft(current_window, frame_length=frame_length, frame_step=frame_step, fft_length=fft_length)
     w, h = S.shape
     frame_step = frame_step
     while h > w:
-      frame_step -= 2
+      frame_step -= 2 # change frame_step to change height of image
       S = tf.signal.stft(current_window, frame_length=frame_length, frame_step=frame_step, fft_length=fft_length)
       w, h = S.shape
 
@@ -86,7 +97,7 @@ def create_stft(current_window, frame_length=255, frame_step=100, fft_length=224
     S = tf.signal.stft(current_window, frame_length=frame_length, frame_step=frame_step, fft_length=fft_length)
     S = librosa.power_to_db(S, ref=np.max)
     w, h = S.shape
-    img = (S-S.min()) / (S.max() - S.min())
+    img = (S-S.min()) / (S.max() - S.min()) # scale image to range of (0, 1) 
 
     if w < h:  # Padding zeros if height < width
       need = h-w
@@ -96,21 +107,24 @@ def create_stft(current_window, frame_length=255, frame_step=100, fft_length=224
       img = img_zer
 
     img = img[:fft_length//2, :fft_length//2]
-    img = np.expand_dims(img, axis=-1)
-    img = np.expand_dims(img, axis=0)
-    return img
+    img = np.expand_dims(img, axis=-1) # add depth dimension
+    img = np.expand_dims(img, axis=0) # add first dimention
+    return img # shape: (1, w, h, 1)
 
+# Convert 1D-raw data to image by mel spectrogram
+# w, h: width, height
+# width = n_mels = 224
 def create_spectrograms_raw(current_window, sample_rate=4000, n_mels=224, f_min=50, f_max=4000, nfft=2048, hop=6): # increase hop -> decrease height of image
     current_window = np.array(current_window)
     S = librosa.feature.melspectrogram(y=current_window, sr=sample_rate, n_mels=n_mels, fmin=f_min, fmax=f_max, n_fft=nfft, hop_length=hop)
     w, h = S.shape
     while h > w:
-      hop += 2
+      hop += 2 # change hop to change height of image
       S = librosa.feature.melspectrogram(y=current_window, sr=sample_rate, n_mels=n_mels, fmin=f_min, fmax=f_max, n_fft=nfft, hop_length=hop)
       w, h = S.shape
     S = librosa.power_to_db(S, ref=np.max)
     w, h = S.shape
-    img = (S-S.min()) / (S.max() - S.min())
+    img = (S-S.min()) / (S.max() - S.min()) # scale image to range of (0, 1) 
 
     if h < w:  # Padding zeros if height < width
       need = w-h
@@ -119,11 +133,12 @@ def create_spectrograms_raw(current_window, sample_rate=4000, n_mels=224, f_min=
       img_zer[:, l: l+h] = S
       img = img_zer
 
-    img = np.expand_dims(img, axis=-1)
-    img = np.expand_dims(img, axis=0)
-    return img
+    img = np.expand_dims(img, axis=-1) # add depth dimension
+    img = np.expand_dims(img, axis=0) # add first dimention
+    return img # shape: (1, w, h, 1)
 
 ############################################################ VALIDATION MATRICES #################################################
+# read data_paper.pdf to understand matrices
 def accuracy_m(y_true, y_pred):
   correct = 0
   total = 0
