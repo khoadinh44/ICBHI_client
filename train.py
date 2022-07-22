cae77a632aa9e6909171ba78904d337a9b2e1e6c
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-07-22 08:06:46 +0700

    Update train.py

--- train.py
+++ train.py
@@ -226,7 +226,7 @@ def train(args):
         pred_label = np.argmax(pred_label, axis=-1)
         cm = confusion_matrix(test_label, pred_label, labels=[0, 1, 2, 3])
         disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=['Normal', 'crackle', 'wheeze', 'both'])
-        plt.savefig(name)
+        plt.savefig('model_' + args.model_name + '_' + args.based_image)
         disp.plot()
         plt.show()
         
